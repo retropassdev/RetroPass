387862a8ad7f72649514d1a544f1fd51ff2b8059
MoistDreams <no-reply@no-reply.com>
2023-02-04 20:20:23 +1100

    Soltuion Name Change 2
    Package ID and version bump.

--- RetroPass/Properties/AssemblyInfo.cs
+++ RetroPass/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RetroPass")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RetroPassEX")]
+[assembly: AssemblyDescription("Retro Pass EX is a simple front-end for emulators on XBOX One/ Series S|X")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RetroPass")]
+[assembly: AssemblyProduct("RetroPassEX")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
