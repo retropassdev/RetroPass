d59eed0cf3fb92d735580a39dd6478178f74e065
MoistDreams <no-reply@no-reply.com>
2023-03-03 18:42:43 +1100

    Create.ThemePage
    Add - page

---
+++ RetroPass/ThemeSettingsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace RetroPass
+{
+    public sealed partial class ThemeSettingsPage : Page
+    {
+        private DataSourceManager dataSourceManager;
+        private Brush defaultForeground;
+
+        public ThemeSettingsPage()
+        {
+            //Instance = this;
+            InitializeComponent();
+            Loaded += ThemeSettingsPage_Loaded;
+        }
+
+        private void ThemeSettingsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
