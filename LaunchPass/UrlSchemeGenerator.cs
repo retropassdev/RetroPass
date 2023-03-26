594122dc0be34cfe52b168b085e621967fcc2926
MoistDreams <no-reply@no-reply.com>
2023-03-27 00:41:16 +1100

    Internal Optimization's
    Improves  the internal comments for better readability.
    Merge some working things from the experimental code.

--- LaunchPass/UrlSchemeGenerator.cs
+++ LaunchPass/UrlSchemeGenerator.cs
@@ -1,13 +1,20 @@
+// This code defines a UrlSchemeGenerator class.
+// The class generates URLs for launching games in different emulators based on the game's emulator type.
+// It supports various emulator types, including Retroarch, Retrix, XBSX2, Dolphin, PPSSPP, Duckstation, Flycast, Xenia, and Xenia Canary.
+
 using System;
 
 namespace RetroPass
 {
+    // Defines an internal class UrlSchemeGenerator.
     internal class UrlSchemeGenerator
     {
+        // Public method GetUrl takes a Game object and returns a URL for launching the game in the appropriate emulator.
         public static string GetUrl(Game game)
         {
             string url = "";
 
+            // Switch case to determine the emulator type for the game.
             switch (game.GamePlatform.EmulatorType)
             {
                 case Platform.EEmulatorType.retroarch:
@@ -53,6 +60,9 @@ namespace RetroPass
             return url;
         }
 
+        // Private methods for generating URLs for different emulator types.
+
+        // GetUrlRetroarch generates a URL for launching a game in the Retroarch emulator.
         private static string GetUrlRetroarch(Game game)
         {
             string args = "cmd=" + "retroarch";
@@ -63,9 +73,10 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlRetrix generates a URL for launching a game in the Retrix emulator.
         private static string GetUrlRetrix(Game game)
         {
-            //retrix uses the same uri scheme syntax as retroarch
+            // Retrix uses the same URI scheme syntax as Retroarch.
             string args = "cmd=" + "retroarch";
             args += " -L";
             args += " cores\\" + game.CoreName;
@@ -74,6 +85,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlXBSX2 generates a URL for launching a game in the XBSX2 (PCSX2) emulator.
         private static string GetUrlXBSX2(Game game)
         {
             string args = "cmd=" + "pcsx2.exe";
@@ -82,6 +94,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlDolphin generates a URL for launching a game in the Dolphin emulator.
         private static string GetUrlDolphin(Game game)
         {
             string args = "cmd=" + "dolphin.exe";
@@ -90,6 +103,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlPpsspp generates a URL for launching a game in the PPSSPP emulator.
         private static string GetUrlPpsspp(Game game)
         {
             string args = "cmd=" + "ppsspp.exe";
@@ -98,6 +112,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlDuckstation generates a URL for launching a game in the Duckstation emulator.
         private static string GetUrlDuckstation(Game game)
         {
             string args = "cmd=" + "duckstation.exe";
@@ -106,6 +121,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlFlycast generates a URL for launching a game in the Flycast emulator.
         private static string GetUrlFlycast(Game game)
         {
             string args = "cmd=" + "flycast.exe";
@@ -114,6 +130,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlXenia generates a URL for launching a game in the Xenia emulator.
         private static string GetUrlXenia(Game game)
         {
             string args = "cmd=" + "xenia.exe";
@@ -122,6 +139,7 @@ namespace RetroPass
             return game.GamePlatform.EmulatorType.ToString() + ":?" + args;
         }
 
+        // GetUrlXeniaCanary generates a URL for launching a game in the Xenia Canary emulator.
         private static string GetUrlXeniaCanary(Game game)
         {
             string args = "cmd=" + "xeniacanary.exe";
