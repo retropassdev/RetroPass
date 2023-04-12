ffa1b2f636f76b3db4e10aa4217798a81fb9a59d
danprice142 <61921129+danprice142@users.noreply.github.com>
2023-04-13 00:57:36 +0100

    Edited Custom Panel

--- LaunchPass/LaunchPassCustomPanel.cs
+++ LaunchPass/LaunchPassCustomPanel.cs
@@ -1,52 +1,66 @@
 ﻿using System;
+using System.Linq;
 using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
 
 namespace LaunchPass
 {
     public class LaunchPassCustomPanel : Panel
     {
-        private double maxWidth;
-        private double maxHeight;
-
         protected override Size ArrangeOverride(Size finalSize)
         {
-            var x = 0.0;
-            var y = 0.0;
+            double y = 0;
+            double rowHeight = 0;
+            double x = 0;
             foreach (var child in Children)
             {
-                if ((maxWidth + x) > finalSize.Width)
+                var desiredSize = child.DesiredSize;
+
+                if ((x + desiredSize.Width) > finalSize.Width)
                 {
+                    y += rowHeight + 4;
                     x = 0;
-                    y += maxHeight;
+                    rowHeight = 0;
                 }
-                var newpos = new Rect(x, y, maxWidth, maxHeight);
-                child.Arrange(newpos);
-                x += maxWidth;
+
+                child.Arrange(new Rect(x, y, desiredSize.Width, desiredSize.Height));
+                x += desiredSize.Width + 4;
+
+                rowHeight = Math.Max(rowHeight, desiredSize.Height);
             }
-            return finalSize;
+
+            return new Size(finalSize.Width, y + rowHeight);
         }
 
         protected override Size MeasureOverride(Size availableSize)
         {
+            double maxWidth = 0;
+            double maxHeight = 0;
+            double currentWidth = 0;
+            double currentRowMaxHeight = 0;
+
             foreach (var child in Children)
             {
                 child.Measure(availableSize);
+                var desiredSize = child.DesiredSize;
 
-                var desirtedwidth = child.DesiredSize.Width;
-                if (desirtedwidth > maxWidth)
-                    maxWidth = desirtedwidth;
+                if (currentWidth + desiredSize.Width > availableSize.Width)
+                {
+                    maxHeight += currentRowMaxHeight + 4;
+                    maxWidth = Math.Max(maxWidth, currentWidth);
+                    currentWidth = 0;
+                    currentRowMaxHeight = 0;
+                }
 
-                var desiredheight = child.DesiredSize.Height;
-                if (desiredheight > maxHeight)
-                    maxHeight = desiredheight;
+                currentWidth += desiredSize.Width + 4;
+                currentRowMaxHeight = Math.Max(currentRowMaxHeight, desiredSize.Height);
             }
 
-            maxWidth = maxWidth <= 0 ? 1 : maxWidth;
+            maxHeight += currentRowMaxHeight;
+            maxWidth = Math.Max(maxWidth, currentWidth > 0 ? currentWidth - 4 : 0);
 
-            var itemperrow = Math.Floor(availableSize.Width / maxWidth);
-            var rows = Math.Ceiling(Children.Count / itemperrow);
-            return new Size(itemperrow * maxWidth, maxHeight * rows);
+            return new Size(maxWidth, maxHeight);
         }
     }
 }
